a0e13be7d2f79bf0da7284bafdfee2b5cc51d141
Iuri Matias <iuri.matias@gmail.com>
2025-03-11 18:34:51 -0400

    cast QBoxLayout, ensure parent is set for push buttons

--- samples/vbox_layout_dynamic/mainwindow.nim
+++ samples/vbox_layout_dynamic/mainwindow.nim
@@ -18,11 +18,11 @@ type
 proc addNewButton(self: MainWindow) =
   echo "addNewButton"
   self.buttonCount += 1
-  let newButton = QPushButton.create("Button " & $self.buttonCount)
+  let newButton = QPushButton.create("Button " & $self.buttonCount, self.window)
   
   # Insert the new button before the "Add Button" button
   let addButtonIndex = self.hboxLayout.indexOf(self.addButton)
-  self.hboxLayout.insertWidget(addButtonIndex, newButton)
+  QBoxLayout(self.hboxLayout).insertWidget(addButtonIndex, newButton)
   
   # Set the stretch factor for the new button
   discard self.hboxLayout.setStretchFactor(newButton, 1)
@@ -48,11 +48,11 @@ proc newMainWindow*(): MainWindow =
   # result.hboxLayout = QBoxLayout.create(QBoxLayoutDirectionEnum.TopToBottom, result.centralWidget)
   
   # Create widgets to add to the layout
-  result.button1 = QPushButton.create("Button 1")
-  result.button2 = QPushButton.create("Button 2")
-  result.button3 = QPushButton.create("Button 3")
-  result.addButton = QPushButton.create("Add Button")
-  result.label = QLabel.create("QhboxLayout arranges widgets vertically from top to bottom")
+  result.button1 = QPushButton.create("Button 1", result.window)
+  result.button2 = QPushButton.create("Button 2", result.window)
+  result.button3 = QPushButton.create("Button 3", result.window)
+  result.addButton = QPushButton.create("Add Button", result.window)
+  result.label = QLabel.create("QhboxLayout arranges widgets vertically from top to bottom", result.window)
   
   # Connect the Add Button to the callback
   # result.addButton.connect(SignalClicked, proc() = addNewButton(result))
@@ -60,11 +60,11 @@ proc newMainWindow*(): MainWindow =
   result.setupDynamicButton()
   
   # Add widgets to the layout
-  result.hboxLayout.addWidget(result.button1)
-  result.hboxLayout.addWidget(result.button2)
-  result.hboxLayout.addWidget(result.button3)
-  result.hboxLayout.addWidget(result.addButton)
-  result.hboxLayout.addWidget(result.label)
+  QBoxLayout(result.hboxLayout).addWidget(result.button1)
+  QBoxLayout(result.hboxLayout).addWidget(result.button2)
+  QBoxLayout(result.hboxLayout).addWidget(result.button3)
+  QBoxLayout(result.hboxLayout).addWidget(result.addButton)
+  QBoxLayout(result.hboxLayout).addWidget(result.label)
   
   # Set stretch factors (optional)
   # This makes the label take up more space than the buttons
